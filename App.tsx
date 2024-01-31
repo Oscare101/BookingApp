3b57ba2f1a27ccabb81f1e81861e8de1088b41e0
Oscare101 <kirillfedortsev2000@gmail.com>
2024-01-31 11:02:03 +0200

    modal gradient, needs fixes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 import MaskedView from '@react-native-masked-view/masked-view'
 import { LinearGradient } from 'expo-linear-gradient'
-import { BlurView } from '@react-native-community/blur'
+import { BlurView, VibrancyView } from '@react-native-community/blur'
+
+const { width, height } = Dimensions.get('screen')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <MaskedView
+      {/* <MaskedView
         style={{}}
         maskElement={
           <Text
@@ -22,22 +23,7 @@ export default function App() {
           </Text>
         }
       >
-        {/* Shows behind the mask, you can put anything here, such as an image */}
-        {/* <View style={{ flex: 1, height: '100%', backgroundColor: '#324376' }} />
-        <View style={{ flex: 1, height: '100%', backgroundColor: '#F5DD90' }} />
-        <View style={{ flex: 1, height: '100%', backgroundColor: '#F76C5E' }} />
-        <View style={{ flex: 1, height: '100%', backgroundColor: '#e1e1e1' }} /> */}
-        <LinearGradient
-          // Background Linear Gradient
-          colors={['#ff0000', '#00ff00', '#0000ff']}
-          style={
-            {
-              // flex: 1,
-              // height: '55%',
-              // width: '100%',
-            }
-          }
-        >
+        <LinearGradient colors={['#ff0000', '#00ff00', '#0000ff']}>
           <Text
             style={{
               fontSize: 60,
@@ -48,13 +34,76 @@ export default function App() {
             Basic Mask\nertyuio
           </Text>
         </LinearGradient>
-      </MaskedView>
-      <BlurView
-        style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}
-        blurType="light"
-        blurAmount={10}
-        reducedTransparencyFallbackColor="white"
+      </MaskedView> */}
+      <Image
+        source={{
+          uri: 'https://papers.co/wallpaper/papers.co-bh38-iceberg-dark-night-sky-art-34-iphone6-plus-wallpaper.jpg',
+        }}
+        height={height}
+        width={width}
+        style={{ zIndex: -1, position: 'absolute' }}
       />
+      {/*  */}
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 20,
+          width: width - 40,
+          alignSelf: 'center',
+          // padding: 30,
+          borderRadius: 30,
+          height: height / 3,
+          // backgroundColor: 'red',
+          overflow: 'hidden',
+          // borderWidth: 1,
+          // borderColor: '#ffffff40',
+        }}
+      >
+        <BlurView
+          style={{
+            flex: 1,
+            width: '100%',
+            position: 'absolute',
+            padding: 20,
+            height: '100%',
+            // backgroundColor: '#FFFFFF10',
+          }}
+          blurType="light"
+          blurAmount={5}
+          reducedTransparencyFallbackColor="white"
+        >
+          <Text
+            style={{
+              fontSize: 36,
+              letterSpacing: 1,
+              fontWeight: '400',
+              paddingBottom: 10,
+            }}
+          >
+            Title
+          </Text>
+          <View
+            style={{
+              width: '100%',
+              borderRadius: 100,
+              padding: 10,
+              backgroundColor: '#D9EEFF90',
+              flexDirection: 'row',
+              alignItems: 'center',
+              paddingHorizontal: 15,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: '300',
+              }}
+            >
+              Search
+            </Text>
+          </View>
+        </BlurView>
+      </View>
     </View>
   )
 }
